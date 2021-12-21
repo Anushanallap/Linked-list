d88b4a520f4abe75eafdf3bb3d953710e8a6f2c0
Anushanallap <anusha.n468@gmail.com>
2021-12-21 08:51:36 +0530

    add MyNode file

---
+++ MyNode.java
@@ -0,0 +1,21 @@
+package linklist;
+
+public class MyNode <k> {
+	
+			private k key;
+			
+			private MyNode next;
+			
+			public MyNode(k key) {
+				this.key = key;
+		        this.next = null;
+			}
+			
+			public MyNode getNext() {
+				return next;
+				
+			}
+		public void setNext(MyNode next) {
+			this.next = next;
+		}
+}
